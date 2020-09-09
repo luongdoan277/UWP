fc61eee0de704ed83529e560b1a2e76105514dcd
luongdoan277 <luongdoan277@gmail.com>
2020-09-09 12:02:07 +0700

    lab7

---
+++ UWP41/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP41.Models
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AvatarPath { get; set; }
+    }
+    public class Icon
+    {
+        public string NameIcon { get; set; }
+        public string IconPath { get; set; }
+    }
+}
