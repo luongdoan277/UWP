83ec02ce13715387c95f377dd921a7c44d664915
luongdoan277 <luongdoan277@gmail.com>
2020-09-15 10:28:16 +0700

    lab9-update

--- UWPSoundBoard/MainPage.xaml.cs
+++ UWPSoundBoard/MainPage.xaml.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Net.Mime;
 using System.Runtime.InteropServices.WindowsRuntime;
 using UWPSoundBoard.Models;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.ApplicationModel.DataTransfer;
 using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -15,6 +16,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -49,9 +51,9 @@ namespace UWPSoundBoard
 
         private void SearchAutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
         {
-            //SoundManager.GetAllSounds(Sounds);
-            //Suggestions = Sounds.Where(p => p.Name.StartsWith(sender.Text)).Select(p => p.Name).ToList();
-            //SearchAutoSuggestBox.ItemsSource = Suggestions;
+            SoundManager.GetAllSounds(Sounds);
+            Suggestions = Sounds.Where(p => p.Name.StartsWith(sender.Text)).Select(p => p.Name).ToList();
+            SearchAutoSuggestBox.ItemsSource = Suggestions;
             if (String.IsNullOrEmpty(sender.Text)) goBack();
         }
 
@@ -96,13 +98,12 @@ namespace UWPSoundBoard
             if (e.DataView.Contains(StandardDataFormats.StorageItems))
             {
                 var items = await e.DataView.GetStorageItemsAsync();
-
                 if (items.Any())
                 {
                     var storageFile = items[0] as StorageFile;
                     var contentType = storageFile.ContentType;
 
-                    StorageFile folder = ApplicationData.Current.LocalFolder;
+                    StorageFolder folder = ApplicationData.Current.LocalFolder;
 
                     if (contentType == "audio/wav" || contentType == "audio/mpeg")
                     {
